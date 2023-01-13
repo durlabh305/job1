79211e6dfa46e5b2b0a24ef8b4510488646b51e5
Durlabh <durlabh305@gmail.com>
2023-01-13 18:05:30 +0530

    First Commit

---
+++ Class1.java
@@ -0,0 +1,14 @@
+package Project_1;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		Class1 t1 = new Class1();
+		t1.method1();
+		
+	}
+	
+	public void method1() {
+		System.out.println("Java program");
+	}
+}
